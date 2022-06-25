dbaf1a79c52f3b17b60a166e217b8233da5c0076
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-06-25 19:49:38 +0200

    feat: Add seeker controls

--- cmd/vintangle-controls/main.go
+++ cmd/vintangle-controls/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"os"
+	"time"
 
 	"github.com/diamondburned/gotk4-adwaita/pkg/adw"
+	"github.com/diamondburned/gotk4/pkg/gdk/v4"
 	"github.com/diamondburned/gotk4/pkg/gio/v2"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
+
+func formatDuration(duration time.Duration) string {
+	hours := math.Floor(duration.Hours())
+	minutes := math.Floor(duration.Minutes()) - (hours * 60)
+	seconds := math.Floor(duration.Seconds()) - (minutes * 60) - (hours * 3600)
+
+	return fmt.Sprintf("%02d:%02d:%02d", int(hours), int(minutes), int(seconds))
+}
 
 func main() {
 	app := adw.NewApplication("com.pojtinger.felicitas.vintanglecontrols", gio.ApplicationFlags(gio.ApplicationFlagsNone))
 
 	app.StyleManager().SetColorScheme(adw.ColorSchemePreferDark)
 
+	prov := gtk.NewCSSProvider()
+	prov.ConnectParsingError(func(section *gtk.CSSSection, err error) {
+		panic(err)
+	})
+	prov.LoadFromData(`.tabular-nums {
+  font-variant-numeric: tabular-nums;
+}`)
+
 	app.ConnectActivate(func() {
+		gtk.StyleContextAddProviderForDisplay(
+			gdk.DisplayGetDefault(),
+			prov,
+			gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+		)
+
 		window := adw.NewApplicationWindow(&app.Application)
 		window.SetTitle("Vintangle - movie.mkv")
 		window.SetDefaultSize(700, 100)
@@ -54,19 +80,37 @@ func main() {
 
 		controls.Append(stopButton)
 
-		leftTrack := gtk.NewLabel("0:28:15")
+		total, err := time.ParseDuration("2h")
+		if err != nil {
+			panic(err)
+		}
+
+		leftTrack := gtk.NewLabel(formatDuration(time.Duration(0)))
 		leftTrack.SetMarginStart(12)
+		leftTrack.AddCSSClass("tabular-nums")
 
 		controls.Append(leftTrack)
 
+		rightTrack := gtk.NewLabel(formatDuration(total))
+		rightTrack.SetMarginEnd(12)
+		rightTrack.AddCSSClass("tabular-nums")
+
 		seeker := gtk.NewScale(gtk.OrientationHorizontal, nil)
-		seeker.SetRange(0, 100)
+		seeker.SetRange(0, float64(total.Nanoseconds()))
 		seeker.SetHExpand(true)
+		seeker.ConnectChangeValue(func(scroll gtk.ScrollType, value float64) (ok bool) {
+			seeker.SetValue(value)
+
+			elapsed := time.Duration(int64(value))
+			remaining := total - elapsed
+
+			leftTrack.SetLabel(formatDuration(elapsed))
+			rightTrack.SetLabel("-" + formatDuration(remaining))
+
+			return true
+		})
 
 		controls.Append(seeker)
-
-		rightTrack := gtk.NewLabel("-0:53:54")
-		rightTrack.SetMarginEnd(12)
 
 		controls.Append(rightTrack)
 
