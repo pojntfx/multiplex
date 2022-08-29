9c12c24c1d6c7876886a47760468629320b193ef
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-08-29 23:57:23 +0200

    refactor: Start decomposing GUI

---
+++ pkg/api/sockets/v1/constants.go
@@ -0,0 +1,6 @@
+package v1
+
+const (
+	MPVTypeSub   = "sub"
+	MPVTypeAudio = "audio"
+)
---
+++ pkg/gschema/flags.go
@@ -0,0 +1,17 @@
+package gschema
+
+const (
+	VerboseFlag = "verbose"
+	StorageFlag = "storage"
+	MPVFlag     = "mpv"
+
+	GatewayRemoteFlag   = "gatewayremote"
+	GatewayURLFlag      = "gatewayurl"
+	GatewayUsernameFlag = "gatewayusername"
+	GatewayPasswordFlag = "gatewaypassword"
+
+	WeronURLFlag        = "weronurl"
+	WeronTimeoutFlag    = "werontimeout"
+	WeronICEFlag        = "weronice"
+	WeronForceRelayFlag = "weronforcerelay"
+)
