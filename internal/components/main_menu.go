8d56951537a0613139b589ae97bb14637259e606
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-08-15 19:00:47 -0700

    docs: Add new release version to about menu
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- internal/components/main_menu.go
+++ internal/components/main_menu.go
@@ -31,7 +31,7 @@ func AddMainMenu(
 	menuBuilder := gtk.NewBuilderFromResource(resources.GResourceMenuPath)
 	menu := menuBuilder.GetObject("main-menu").Cast().(*gio.Menu)
 
-	aboutDialog := adw.NewAboutDialogFromAppdata(resources.GResourceMetainfoPath, "0.1.4")
+	aboutDialog := adw.NewAboutDialogFromAppdata(resources.GResourceMetainfoPath, "0.1.5")
 	aboutDialog.SetDevelopers([]string{"Felicitas Pojtinger"})
 	aboutDialog.SetArtists([]string{"Brage Fuglseth"})
 	aboutDialog.SetCopyright("© 2024 Felicitas Pojtinger")
