6014d2ae97878bbdf2f74d9be57bc6a8c20988e5
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-11-19 23:42:23 -0800

    feat: Port `ErrorDialog` to use subclassing
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- internal/components/error_dialog.go
+++ internal/components/error_dialog.go
@@ -1,36 +1,59 @@
 package components
 
 import (
-	. "github.com/pojntfx/go-gettext/pkg/i18n"
-
 	"context"
 	"os"
+	"runtime"
+	"unsafe"
+
+	. "github.com/pojntfx/go-gettext/pkg/i18n"
 
 	"github.com/jwijenbergh/puregotk/v4/adw"
+	"github.com/jwijenbergh/puregotk/v4/glib"
+	"github.com/jwijenbergh/puregotk/v4/gobject"
 	"github.com/jwijenbergh/puregotk/v4/gtk"
 	"github.com/pojntfx/multiplex/assets/resources"
 	"github.com/rs/zerolog/log"
 	"github.com/rymdport/portal/openuri"
 )
 
+var (
+	gTypeErrorDialog gobject.Type
+)
+
 const (
 	issuesURL = "https://github.com/pojntfx/multiplex/issues"
 )
+
+type ErrorDialog struct {
+	adw.AlertDialog
+
+	responseCallback func(response string)
+}
+
+func NewErrorDialog() ErrorDialog {
+	obj := gobject.NewObject(gTypeErrorDialog, "css-name")
+
+	var v ErrorDialog
+	obj.Cast(&v)
+
+	return v
+}
+
+func (e *ErrorDialog) SetResponseCallback(callback func(response string)) {
+	errD := (*ErrorDialog)(unsafe.Pointer(e.GetData(dataKeyGoInstance)))
+	errD.responseCallback = callback
+}
 
 func OpenErrorDialog(ctx context.Context, window *adw.ApplicationWindow, err error) {
 	log.Error().
 		Err(err).
 		Msg(L("Could not continue due to a fatal error"))
 
-	errorBuilder := gtk.NewBuilderFromResource(resources.ResourceErrorPath)
-	defer errorBuilder.Unref()
-	var errorDialog adw.AlertDialog
-	errorBuilder.GetObject("error-dialog").Cast(&errorDialog)
-	defer errorDialog.Unref()
-
+	errorDialog := NewErrorDialog()
 	errorDialog.SetBody(err.Error())
 
-	responseCallback := func(dialog adw.AlertDialog, response string) {
+	responseCallback := func(response string) {
 		switch response {
 		case "report":
 			_ = openuri.OpenURI("", issuesURL, nil)
@@ -45,7 +68,60 @@ func OpenErrorDialog(ctx context.Context, window *adw.ApplicationWindow, err err
 			os.Exit(1)
 		}
 	}
-	errorDialog.ConnectResponse(&responseCallback)
+	errorDialog.SetResponseCallback(responseCallback)
 
 	errorDialog.Present(&window.Widget)
+}
+
+func init() {
+	var classInit gobject.ClassInitFunc = func(tc *gobject.TypeClass, u uintptr) {
+		typeClass := (*gtk.WidgetClass)(unsafe.Pointer(tc))
+		typeClass.SetTemplateFromResource(resources.ResourceErrorPath)
+
+		objClass := (*gobject.ObjectClass)(unsafe.Pointer(tc))
+
+		objClass.OverrideConstructed(func(o *gobject.Object) {
+			parentObjClass := (*gobject.ObjectClass)(unsafe.Pointer(tc.PeekParent()))
+			parentObjClass.GetConstructed()(o)
+
+			var parent adw.AlertDialog
+			o.Cast(&parent)
+
+			parent.InitTemplate()
+
+			e := &ErrorDialog{
+				AlertDialog: parent,
+			}
+
+			responseCallback := func(dialog adw.AlertDialog, response string) {
+				if e.responseCallback != nil {
+					e.responseCallback(response)
+				}
+			}
+			parent.ConnectResponse(&responseCallback)
+
+			var pinner runtime.Pinner
+			pinner.Pin(e)
+
+			var cleanupCallback glib.DestroyNotify = func(data uintptr) {
+				pinner.Unpin()
+			}
+			o.SetDataFull(dataKeyGoInstance, uintptr(unsafe.Pointer(e)), &cleanupCallback)
+		})
+	}
+
+	var instanceInit gobject.InstanceInitFunc = func(ti *gobject.TypeInstance, tc *gobject.TypeClass) {}
+
+	var parentQuery gobject.TypeQuery
+	gobject.NewTypeQuery(adw.AlertDialogGLibType(), &parentQuery)
+
+	gTypeErrorDialog = gobject.TypeRegisterStaticSimple(
+		parentQuery.Type,
+		"ErrorDialog",
+		parentQuery.ClassSize,
+		&classInit,
+		parentQuery.InstanceSize+uint(unsafe.Sizeof(ErrorDialog{}))+uint(unsafe.Sizeof(&ErrorDialog{}))+uint(unsafe.Sizeof(adw.AlertDialog{})), // TODO: Figure out why we need the extra `adw.AlertDialog` here
+		&instanceInit,
+		0,
+	)
 }
